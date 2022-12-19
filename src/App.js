094d418b89625459d77bd68c24be91aee7069cd6
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-12-19 10:09:10 +0000

    created pages and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Explore from "./Pages/Explore";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      <Navbar />
     </>
   );
 }
